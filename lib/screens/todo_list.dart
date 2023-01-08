18cff7fabbc4aee73668ade0ce0aca93e6549dbd
Abdul Basit Mehtab <abasitm.1993@gmail.com>
2023-01-08 21:50:04 +0500

    Added Flutter CI/CD Action Workflow  +  Working on delete functionality

--- lib/screens/todo_list.dart
+++ lib/screens/todo_list.dart
@@ -36,10 +36,27 @@ class _TodoListPageState extends State<TodoListPage> {
               itemCount: items.length,
               itemBuilder: (context, index) {
                 final item = items[index] as Map;
+                final id = item['_id'] as String;
                 return ListTile(
                   leading: CircleAvatar(child: Text('${index + 1}')),
                   title: Text(item['title']),
                   subtitle: Text(item['content']),
+                  trailing: PopupMenuButton(onSelected: (value) {
+                    if (value == 'edit') {
+                      // Open Edit Screen
+                    } else if (value == 'delete') {
+                      // Delete and remove the item
+                      deleteById(id);
+                    }
+                  }, itemBuilder: (context) {
+                    return [
+                      PopupMenuItem(child: Text('Edit'), value: 'edit'),
+                      PopupMenuItem(
+                        child: Text('Delete'),
+                        value: 'delete',
+                      ),
+                    ];
+                  }),
                 );
               }),
         ),
@@ -54,6 +71,20 @@ class _TodoListPageState extends State<TodoListPage> {
       builder: (context) => AddTodoPage(),
     );
     Navigator.push(context, route);
+  }
+
+  Future<void> deleteById(String id) async {
+    // Delete the item
+    final url =
+        'https://amused-tank-top-bee.cyclic.app/api/todos/deleteTodo/${id}';
+    final uri = Uri.parse(url);
+    final response = await http.delete(uri);
+    if (response.statusCode == 200) {
+      // Remove the item from the list
+
+    } else {
+      // Show error
+    }
   }
 
   Future<void> fetchTodo() async {
